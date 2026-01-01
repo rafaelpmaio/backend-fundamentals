98915c55d21640b71d63ec66dcc6133a0e93955f
rafaelpmaio <rafaelp91@gmail.com>
2026-01-01 17:49:16 -0300

    feat: adicionar middleware de log global
    - Aplicar loggerMiddleware em todas as requisições
    - Melhorar rastreamento de requisições HTTP

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import type { Application, Request, Response } from 'express';
 import userRoutes from './routes/user.routes.js'
+import { loggerMiddleware } from './middlewares/logger.middleware.js';
 
 const app: Application = express();
+
+app.use(loggerMiddleware);
 
 app.use(express.json());
 app.use('/users', userRoutes);
 
-app.get('/', (req: Request, res: Response) =>{
-res.status(200).json({message: 'Backend Fundamentals API rodando'});
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Backend Fundamentals API rodando' });
 });
 
 app.use((req: Request, res: Response) => {
