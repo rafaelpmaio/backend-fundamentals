036012f024fe88fdddd297ae2e4e9b658147f3f4
Rafael Maio <120125116+rafaelpmaio@users.noreply.github.com>
2026-01-16 00:50:05 +0000

    feat: deletado arquivo auth.middleware.ts
    - Substituido validação em user.routes.ts por passport.middleware.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,15 +6,15 @@ import {
     getUsers,
     getUserById
 } from '../controllers/user.controller.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { isAuthenticated } from '../middlewares/passport.middleware.js';
 import { validateIdParam, validateUserBody, validateUpdateBody } from '../middlewares/validation.middleware.js';
 
 const router = Router();
 
-router.get('/', authMiddleware, getUsers);
-router.get('/:id', authMiddleware, validateIdParam, getUserById);
-router.post('/', authMiddleware, validateUserBody, createUser);
-router.put('/:id', authMiddleware, validateIdParam, validateUpdateBody, updateUser);
-router.delete('/:id', authMiddleware, validateIdParam, deleteUser);
+router.get('/', isAuthenticated, getUsers);
+router.get('/:id', isAuthenticated, validateIdParam, getUserById);
+router.post('/', isAuthenticated, validateUserBody, createUser);
+router.put('/:id', isAuthenticated, validateIdParam, validateUpdateBody, updateUser);
+router.delete('/:id', isAuthenticated, validateIdParam, deleteUser);
 
 export default router;
