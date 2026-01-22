b2a2f3d1039b9f1714e820c85381c0baeab03744
rafaelpmaio <rafaelp91@gmail.com>
2026-01-22 11:54:13 -0300

    refactor(user.routes): utiliza instancia de user.controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,20 +1,14 @@
 import { Router } from 'express';
-import {
-    createUser,
-    updateUser,
-    deleteUser,
-    getUsers,
-    getUserById
-} from '../controllers/user.controller.js';
+import { userController } from '../controllers/user.controller.js';
 import { isAuthenticated } from '../middlewares/passport.middleware.js';
 import { validateIdParam, validateUserBody, validateUpdateBody } from '../middlewares/validation.middleware.js';
 
 const router = Router();
 
-router.get('/', isAuthenticated, getUsers);
-router.get('/:id', isAuthenticated, validateIdParam, getUserById);
-router.post('/', isAuthenticated, validateUserBody, createUser);
-router.put('/:id', isAuthenticated, validateIdParam, validateUpdateBody, updateUser);
-router.delete('/:id', isAuthenticated, validateIdParam, deleteUser);
+router.get('/', isAuthenticated, userController.getUsers);
+router.get('/:id', isAuthenticated, validateIdParam, userController.getUserById);
+router.post('/', isAuthenticated, validateUserBody, userController.createUser);
+router.put('/:id', isAuthenticated, validateIdParam, validateUpdateBody, userController.updateUser);
+router.delete('/:id', isAuthenticated, validateIdParam, userController.deleteUser);
 
 export default router;
