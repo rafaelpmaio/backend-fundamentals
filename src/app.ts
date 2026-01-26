0aad26af192088ec8e8252c4bc11b262a1c352fb
rafaelpmaio <rafaelp91@gmail.com>
2026-01-26 11:22:43 -0300

    refactor(app.ts): finalizando migração de session para jwt

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,17 @@
 import express from 'express';
-import session from 'express-session';
 import type { Application, Request, Response, NextFunction } from 'express';
 import passport from './config/passport.config.js';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import { loggerMiddleware } from './middlewares/logger.middleware.js';
-import { ENV } from './config/env.js';
 
 const app: Application = express();
 
 app.use(loggerMiddleware);
 app.use(express.json());
 
-app.use(
-    session({
-        secret: ENV.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: ENV.SESSION_SECRET === 'production',
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000 // 24 horas
-        }
-    })
-);
-
 // Inicializa passport
 app.use(passport.initialize());
-app.use(passport.session());
 
 //Rotas
 app.use('/auth', authRoutes);
